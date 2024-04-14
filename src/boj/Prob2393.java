7a7c1d9410ad85f9f28383b9aa44adc72a78b432
aquashdw <aquashdw@gmail.com>
2024-04-14 21:25:02 +0900

    boj 2393

---
+++ src/boj/Prob2393.java
@@ -0,0 +1,24 @@
+package boj;
+
+// https://www.acmicpc.net/problem/2393
+public class Prob2393 {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                  ___  ___  ___
+                  | |__| |__| |
+                  |           |
+                   \\_________/
+                    \\_______/
+                     |     |
+                     |     |
+                     |     |
+                     |     |
+                     |_____|
+                  __/       \\__
+                 /             \\
+                /_______________\\
+                """);
+    }
+
+}
