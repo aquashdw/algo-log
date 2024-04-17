301f8d8c28f1ec641aa4595621d48abbe08a00fa
aquashdw <aquashdw@gmail.com>
2024-04-17 09:04:10 +0900

    boj 11279

---
+++ src/boj/Prob11279.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// https://www.acmicpc.net/problem/11279
+public class Prob11279 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        StringBuilder answer = new StringBuilder();
+        reader.lines().limit(n).mapToInt(Integer::parseInt).forEach(i -> {
+            if (i == 0) {
+                if (queue.isEmpty()) answer.append(0).append('\n');
+                else answer.append(queue.poll()).append('\n');
+            } else {
+                queue.offer(i);
+            }
+        });
+        System.out.print(answer);
+    }
+}
