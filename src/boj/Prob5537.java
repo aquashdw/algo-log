210f0185dc9281f80e5a81ecbf13334957e7605a
aquashdw <aquashdw@gmail.com>
2024-05-08 15:10:59 +0900

    boj 5537

---
+++ src/boj/Prob5537.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.IOException;
+
+// https://www.acmicpc.net/problem/5537
+public class Prob5537 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                .  .   .
+                |  | _ | _. _ ._ _  _
+                |/\\|(/.|(_.(_)[ | )(/.
+                """);
+    }
+}
