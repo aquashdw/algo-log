9278e210991b11336f7eb28e41ba4e0a5c9759b5
aquashdw <aquashdw@gmail.com>
2024-05-09 10:14:43 +0900

    boj 5338

---
+++ src/boj/Prob5338.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.IOException;
+
+// https://www.acmicpc.net/problem/5338
+public class Prob5338 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                       _.-;;-._
+                '-..-'|   ||   |
+                '-..-'|_.-;;-._|
+                '-..-'|   ||   |
+                '-..-'|_.-''-._|
+                """);
+    }
+}
