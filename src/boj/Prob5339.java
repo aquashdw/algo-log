2af6e4bc1c525aed93bea2a7ecb2ce5813d2daff
aquashdw <aquashdw@gmail.com>
2024-05-23 14:02:15 +0900

    boj 5339

---
+++ src/boj/Prob5339.java
@@ -0,0 +1,21 @@
+package boj;
+
+// https://www.acmicpc.net/problem/5339
+public class Prob5339 {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                     /~\\
+                    ( oo|
+                    _\\=/_
+                   /  _  \\
+                  //|/.\\|\\\\
+                 ||  \\ /  ||
+                ============
+                |          |
+                |          |
+                |          |
+                """);
+    }
+}
+
