2c761965bd4eb85ed27a82235cbb53c63ff0a39a
aquashdw <aquashdw@gmail.com>
2024-06-03 13:36:56 +0900

    boj 17626

---
+++ src/boj/Prob17626.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/17626
+public class Prob17626 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
