d6a531d4740eddcfe9724f8165345df7bd965e3b
aquashdw <aquashdw@gmail.com>
2024-07-14 18:53:30 +0900

    boj 10189

---
+++ src/boj/Prob10189.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.IOException;
+
+public class Prob10189 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                #  # #### #### #  #
+                #### #  # #  # # #
+                #### #  # #  # # #
+                #  # #### #### #  #
+                """);
+    }
+}
