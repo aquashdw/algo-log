64e27843b6eefa24f1f837dd195b7c7e6efb661f
aquashdw <aquashdw@gmail.com>
2024-12-03 20:25:44 -0800

    boj 8370

---
+++ src/boj/Prob8370.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// https://www.acmicpc.net/problem/8370
+public class Prob8370 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] info = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        System.out.println(info[0] * info[1] + info[2] * info[3]);
+    }
+}
