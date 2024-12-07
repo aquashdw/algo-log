3c4a50bc922496f832403ebb50626c544c547a51
aquashdw <aquashdw@gmail.com>
2024-12-06 21:51:41 -0800

    boj 10718

---
+++ src/boj/Prob10718.java
@@ -0,0 +1,12 @@
+package boj;
+
+// https://www.acmicpc.net/problem/10718
+public class Prob10718 {
+    public static void main(String[] args)
+    {
+        System.out.println("""
+                강한친구 대한육군
+                강한친구 대한육군
+                """);
+    }
+}
