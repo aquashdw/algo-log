7622ca4b88ed0389e4d9c9d80a2e8ad0e4df3fd0
aquashdw <aquashdw@gmail.com>
2024-12-07 19:23:53 -0800

    boj 11942

---
+++ src/boj/Prob11942.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.io.IOException;
+
+// https://www.acmicpc.net/problem/11942
+public class Prob11942 {
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("고려대학교");
+    }
+}
