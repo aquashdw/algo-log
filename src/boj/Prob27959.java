e7e27c48a6b974d9019541aeb8ea6de5ef639434
aquashdw <aquashdw@gmail.com>
2025-01-29 12:06:58 +0900

    boj 27959

---
+++ src/boj/Prob27959.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// https://www.acmicpc.net/problem/27959
+public class Prob27959 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] nm = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        System.out.println(nm[0] * 100 >= nm[1] ? "Yes" : "No");
+    }
+}
