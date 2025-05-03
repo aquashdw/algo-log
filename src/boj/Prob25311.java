c8f3755ea7167f04f2f35f9c7cb57cdbe35282e0
aquashdw <aquashdw@gmail.com>
2025-05-03 21:15:45 +0900

    boj 25311

---
+++ src/boj/Prob25311.java
@@ -0,0 +1,12 @@
+package boj;
+
+import java.io.IOException;
+
+// https://www.acmicpc.net/problem/25311
+public class Prob25311 {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println('A');
+    }
+}
