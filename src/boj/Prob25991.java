a803d11ae3c8d72d7a2ebd410d492c99991c54e5
aquashdw <aquashdw@gmail.com>
2025-07-23 08:08:38 +0900

    boj 25991

---
+++ src/boj/Prob25991.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// https://www.acmicpc.net/problem/25991
+public class Prob25991 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n =  Integer.parseInt(reader.readLine());
+        double cube = Arrays.stream(reader.readLine().split(" "))
+                .mapToDouble(Double::parseDouble)
+                .map(side -> side * side * side)
+                .sum();
+        System.out.println(Math.cbrt(cube));
+    }
+}
