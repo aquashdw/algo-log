9ff4485d680fff1ee2042f45e8c2f677a01b8c1e
aquashdw <aquashdw@gmail.com>
2025-10-21 13:32:44 +0900

    boj 32405

---
+++ src/boj/Prob32405.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// https://www.acmicpc.net/problem/32405
+public class Prob32405 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        Queue<Integer> next = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            next.add(i);
+        }
+        int[] power = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] health = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] deals = new int[n];
+
+        int total = 0;
+
+        while (next.size() > 1) {
+            int now = next.poll();
+            if (health[now] - total + power[now] * deals[now] < 0) continue;
+            total += power[now];
+            deals[now]++;
+            next.offer(now);
+        }
+        System.out.println(next.poll() + 1);
+    }
+
+}
