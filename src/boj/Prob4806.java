5264e546510ecf8073686262c5919aef15cfe495
aquashdw <aquashdw@gmail.com>
2026-02-06 08:59:56 +0900

    boj 4806

---
+++ src/boj/Prob4806.java
@@ -0,0 +1,13 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/4806
+public class Prob4806 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(reader.lines().count());
+    }
+}
