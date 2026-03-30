422cceb36236ccfc73a46ed730e92a5089e2ce6d
aquashdw <aquashdw@gmail.com>
2026-03-30 12:08:25 +0900

    boj 24263

---
+++ src/boj/Prob24263.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/24263
+public class Prob24263 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        System.out.printf("%d%n1%n", n);
+    }
+}
